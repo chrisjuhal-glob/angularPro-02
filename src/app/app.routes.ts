cc7ed58a11d3679e12e36b4c749e817fc53fb242
Christian Daniel Juhal <christian.juhal@globant.com>
2026-04-21 15:43:05 -0300

    SPA init

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'about',
+        loadComponent: () => import('./pages/about/about-page')
+    },
+    {
+        path: 'contact',
+        loadComponent: () => import('./pages/contact/contact-page')
+    },
+    {
+        path: 'pricing',
+        loadComponent: () => import('./pages/pricing/pricing-page')
+    },
+    {
+        path: '',
+        redirectTo: 'about',
+        pathMatch: 'full'
+    }
+];
