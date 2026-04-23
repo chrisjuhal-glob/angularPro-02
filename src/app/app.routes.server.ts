c8c1a57dabf7e7375fc4f27f702086eb01fdea59
Christian Daniel Juhal <christian.juhal@globant.com>
2026-04-23 18:08:47 -0300

    new config for static pages

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,12 +1,40 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
+  // ✅ Home (optional)
   {
-    path: '**',
+    path: '',
     renderMode: RenderMode.Prerender,
   },
+
+  // ✅ Pagination (10 pages)
   {
-    path: 'pokemon/:id',
+    path: 'pokemons/page/:page',
+    renderMode: RenderMode.Prerender,
+    async getPrerenderParams() {
+      return Array.from({ length: 10 }, (_, i) => ({
+        page: `${i + 1}`,
+      }));
+    },
+  },
+
+  // ✅ Pokemon detail (151 pages)
+  {
+    path: 'pokemon/:name',
+    renderMode: RenderMode.Prerender,
+    async getPrerenderParams() {
+      const res = await fetch('https://pokeapi.co/api/v2/pokemon?limit=151');
+      const data = await res.json();
+
+      return data.results.map((p: any) => ({
+        name: p.name,
+      }));
+    },
+  },
+
+  // ✅ Fallback SSR (important)
+  {
+    path: '**',
     renderMode: RenderMode.Server,
   },
 ];
